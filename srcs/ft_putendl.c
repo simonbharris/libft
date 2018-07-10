84fc06cba6c42c70e95cc98bcacf5355e1b35980
Simon Harris <simonbharris@gmail.com>
2018-07-09 20:11:20 -0700

    ft_putnbr fix, submit ft_putendl

--- srcs/ft_putendl.c
+++ srcs/ft_putendl.c
@@ -5,32 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sharris <sharris@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/09 16:26:15 by sharris           #+#    #+#             */
-/*   Updated: 2018/07/09 16:26:15 by sharris          ###   ########.fr       */
+/*   Created: 2018/07/09 20:06:32 by sharris           #+#    #+#             */
+/*   Updated: 2018/07/09 20:06:32 by sharris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	ft_putnbr(int n)
+void	ft_putendl(char const *s)
 {
-	if (n == -2147483648)
-	{
-		ft_putnbr(-214748364);
-		ft_putchar('8');
-	}
-	else if (n < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(n * -1);
-	}
-	else if (n < 10)
-	{
-		ft_putchar(n + '0');
-	}
-	else
-	{
-		ft_putnbr(n / 10);
-		ft_putchar(n % 10 + '0');
-	}
+	ft_putstr(s);
+	ft_putchar('\n');
 }
