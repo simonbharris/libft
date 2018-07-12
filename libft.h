9d37d434d8c4e6164b1921bc16acede628a1b9f4
Simon Harris <simonbharris@gmail.com>
2018-07-12 14:53:09 -0700

    Reorganized header file to allow easier checking for missing functions

--- libft.h
+++ libft.h
@@ -17,6 +17,29 @@
 # include <string.h>
 # include <stdlib.h>
 
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strcat(char *s1, const char *s2);
+
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*strmap(char const *s, char(*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
+char	**ft_strsplit(char const *s, char c);
+char	*ft_itoa(int n);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
 void	ft_putstr(char const *s);
@@ -26,25 +49,6 @@ void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-char	*ft_itoa(int n);
 size_t	ft_strsize(char const *str);
-char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strcat(char *s1, const char *s2);
-char	*ft_strjoin(char const *s1, char const *s2);
-
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_striter(char *s, void (*f)(char *));
-char	*strmap(char const *s, char(*f)(char));
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int		ft_strequ(char const *s1, char const *s2);
-int		ft_strnequ(char const *s1, char const *s2, size_t n);
-char	*ft_strsub(char const *s, unsigned int start, size_t len);
-
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memalloc(size_t size);
-void	ft_memdel(void **ap);
-
-char	*ft_strnew(size_t size);
-void	ft_strclr(char *s);
 
 #endif
