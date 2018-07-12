e9d42d822105286b288f4f093046318f5a87bf1e
Simon Harris <simonbharris@gmail.com>
2018-07-12 15:44:59 -0700

    submitting ft_memccpy+ft_memmove

--- libft.h
+++ libft.h
@@ -20,6 +20,8 @@
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strcat(char *s1, const char *s2);
