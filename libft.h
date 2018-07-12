6331fd7f88919ac6b7c9a63e4090455eb877b17f
Simon Harris <simonbharris@gmail.com>
2018-07-12 16:20:00 -0700

    ft_memchr + ft_memcmp submit

--- libft.h
+++ libft.h
@@ -22,6 +22,8 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strcat(char *s1, const char *s2);
