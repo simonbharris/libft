82fa50f2fcc5ef27ad07bb2b423ec19688887c89
Simon Harris <simonbharris@gmail.com>
2018-07-12 18:02:23 -0700

    submitting strlcat *relieves stress ball*

--- libft.h
+++ libft.h
@@ -27,7 +27,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char*s);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strncpy(char *dst, const char *src, size_t len)
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 
