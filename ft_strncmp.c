ecaefb1ca3cfe86be6fef742ba25b42a8eb3e701
Simon Harris <simonbharris@gmail.com>
2018-07-16 22:41:28 -0700

    norm fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,7 +19,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (!*s1 || !*s2)
 		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	while (i++ < (int) n && s1[i] && s2[i])
+	while (i++ < (int)n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
